c3bbe62673c00355eed438a95b6e47a4e1964af9
Paul Gamble <pgamble@gmail.com>
2019-02-25 01:57:19 -0500

    removed unused class and element to resolve chrome dev warnings

--- src/components/MainForm.jsx
+++ src/components/MainForm.jsx
@@ -3,39 +3,7 @@ import UserDetails from './UserDetails';
 import GoalDetails from './GoalDetails';
 import ShareDetails from './ShareDetails';
 import Success from './Success';
-import { Step } from 'semantic-ui-react';
 
-
-
-class UserSteps extends Component {
-
-    render() {
-        const { values } = this.props;
-        return (
-            <Step.Group ordered>
-                <Step completed>
-                    <Step.Content>
-                        <Step.Title>Shipping</Step.Title>
-                        <Step.Description>Choose your shipping options</Step.Description>
-                    </Step.Content>
-                </Step>
-
-                <Step completed>
-                    <Step.Content>
-                        <Step.Title>Billing</Step.Title>
-                        <Step.Description>Enter billing information</Step.Description>
-                    </Step.Content>
-                </Step>
-
-                <Step active>
-                    <Step.Content>
-                        <Step.Title>Confirm Order</Step.Title>
-                    </Step.Content>
-                </Step>
-            </Step.Group>
-        )
-    }
-}
 
 class MainForm extends Component {
     state = {
