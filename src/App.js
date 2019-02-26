9abf429729e4f1e8539717ada72c661dab4726fa
Paul Gamble <pgamble@gmail.com>
2019-02-26 13:53:40 -0500

    Moved html for index.html into actual components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
+import Header from './components/layout/Header';
+import Content from './components/layout/Content';
 import MainForm from './components/MainForm';
 import { Container } from 'semantic-ui-react';
 
@@ -8,6 +9,8 @@ class App extends Component {
   render() {
     return(
       <Container id="container" textAlign='center' >
+        <Header />
+        <Content />
         <MainForm />
       </Container>    
     )
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Header() {
+  return (
+  
+        <div id="top-menu" class="ui menu">
+            <span class="item">705-725-1818</span>
+            <a class="item" href="mailto:info@barriefoodbank.org">info@barriefoodbank.org</a>
+            <a class="item" href="https://www.facebook.com/barriefoodbank/" > Facebook</a>
+            <a class="item" href="https://twitter.com/barriefoodbank" > <span>Twitter</span></a>
+            <a class="item" href="https://www.barriefoodbank.org/news-blog/">News &amp; Blog</a>
+            <a class="item" href="https://www.barriefoodbank.org/contact/">Contact</a>
+            <a class="item" href="https://www.barriefoodbank.org/donating/">Donating</a>
+        </div>
+
+  )
+}
+
+export default Header;
