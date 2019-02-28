f859eb544fea2ebdfc601d739b40c6e801d4de21
Paul Gamble <pgamble@gmail.com>
2019-02-27 22:59:48 -0500

    changed class to className

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -3,14 +3,12 @@ import React from 'react'
 export default function Content() {
   return (
     <div className="ui inverted vertical masthead center aligned segment">
-
       <div className="ui text container">
-        <h1 className="ui inverted header">
-                    Run a Food Drive
-          </h1>
-                <h2>Make a Difference in the Lives of Others</h2>
-        <div className="ui huge primary button">Get Started</div><i class="right arrow icon"></i></div>
-        </div>
-
+        <h1 className="ui inverted header">Run a Food Drive</h1>
+        <h2>Make a Difference in the Lives of Others</h2>
+        <div className="ui huge primary button">Get Started</div>
+        <i className="right arrow icon"></i>
+      </div>
+    </div>
   )
 }
