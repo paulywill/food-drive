ba8c021f42eef01f8ba21b9053efd93aee4018eb
Paul Gamble <pgamble@gmail.com>
2019-02-27 22:54:06 -0500

    changed class to className

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 export default function Content() {
   return (
-        <div class="ui inverted vertical masthead center aligned segment">
+    <div className="ui inverted vertical masthead center aligned segment">
 
-            <div class="ui text container">
-                <h1 class="ui inverted header">
+      <div className="ui text container">
+        <h1 className="ui inverted header">
                     Run a Food Drive
           </h1>
                 <h2>Make a Difference in the Lives of Others</h2>
-                <div class="ui huge primary button">Get Started</div><i class="right arrow icon"></i></div>
+        <div className="ui huge primary button">Get Started</div><i class="right arrow icon"></i></div>
         </div>
 
   )
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 function Header() {
   return (
     <header>
-        <div id="top-menu" class="ui menu">
-            <span class="item">705-725-1818</span>
-            <a class="item" href="mailto:info@barriefoodbank.org">info@barriefoodbank.org</a>
-            <a class="item" href="https://www.facebook.com/barriefoodbank/" > Facebook</a>
-            <a class="item" href="https://twitter.com/barriefoodbank" > <span>Twitter</span></a>
-            <a class="item" href="https://www.barriefoodbank.org/news-blog/">News &amp; Blog</a>
-            <a class="item" href="https://www.barriefoodbank.org/contact/">Contact</a>
-            <a class="item" href="https://www.barriefoodbank.org/donating/">Donating</a>
+        <div id="top-menu" className="ui menu">
+        <span className="item">705-725-1818</span>
+        <a className="item" href="mailto:info@barriefoodbank.org">info@barriefoodbank.org</a>
+        <a className="item" href="https://www.facebook.com/barriefoodbank/" > Facebook</a>
+        <a className="item" href="https://twitter.com/barriefoodbank" > <span>Twitter</span></a>
+        <a className="item" href="https://www.barriefoodbank.org/news-blog/">News &amp; Blog</a>
+        <a className="item" href="https://www.barriefoodbank.org/contact/">Contact</a>
+        <a className="item" href="https://www.barriefoodbank.org/donating/">Donating</a>
         </div>
     </header>
   )
